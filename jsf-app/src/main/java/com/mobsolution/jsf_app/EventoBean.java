a3dd3f09f9783dd970afe6b1c1d4ec7393fde287
JoaolimaDev <ozymandiasphp@gmail.com>
2025-03-02 21:33:36 -0300

    feat: data table completo

--- jsf-app/src/main/java/com/mobsolution/jsf_app/EventoBean.java
+++ jsf-app/src/main/java/com/mobsolution/jsf_app/EventoBean.java
@@ -2,6 +2,8 @@ package com.mobsolution.jsf_app;
 
 import com.mobsolution.jsf_app.dto.EventDTO;
 import com.mobsolution.jsf_app.dto.EventoResponse;
+import com.mobsolution.jsf_app.dto.ResquestDTO;
+
 import jakarta.annotation.PostConstruct;
 import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
@@ -20,8 +22,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-
 import lombok.Getter;
 import lombok.Setter;
 import java.util.stream.Collectors;
@@ -42,23 +44,110 @@ public class EventoBean implements Serializable {
     private List<EventDTO> filteredEvents; // Store filtered results
 
 
-    private String selectedLink;
+    @Getter @Setter
+    private EventDTO selectedEvent;
+    
 
 
-    public void prepararFormulario() {
-        FacesContext context = FacesContext.getCurrentInstance();
-        Map<String, String> params = context.getExternalContext().getRequestParameterMap();
-        selectedLink = params.get("selectedLink");
+
+    public void prepararFormulario(EventDTO eventDTO) {
+
+        this.selectedEvent = eventDTO;
         
-        System.out.println("Link selecionado: " + selectedLink);
-        // Adicione aqui a lógica adicional
+        if (selectedEvent != null) {
+            System.out.println("Evento selecionado: " + selectedEvent.getNome());
+        } else {
+            System.out.println("Erro: selectedEvent está null");
+        }
+      
     }
 
+
+    public void novoEvento() {
+        this.selectedEvent = new EventDTO();
+    }
+
+    
+    public void salvar(){
+  
+    
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.valueOf("application/hal+json")));
+        headers.setContentType(MediaType.APPLICATION_JSON);  // Set Content-Type to JSON
+        String url = "";
+    
+        if (selectedEvent.get_links() != null) {
+             // Extract event ID from the URL
+            String href = selectedEvent.get_links().getEvento().getHref();
+            String eventId = href.substring(href.lastIndexOf("/evento/") + 8);
+            url = "http://localhost:8080/api/evento/" + eventId;
+        }
+
+    
+        // Create the request body
+        ResquestDTO requestBody = new ResquestDTO(selectedEvent.getNome(), selectedEvent.getDataInicio().toString(), selectedEvent.getDataFim().toString());
+    
+        // Extract JWT from cookies and add to headers
+        FacesContext fc = FacesContext.getCurrentInstance();
+        HttpServletRequest req = (HttpServletRequest) fc.getExternalContext().getRequest();
+        if (req.getCookies() != null) {
+            for (Cookie cookie : req.getCookies()) {
+                if ("jwt".equals(cookie.getName())) {
+                    headers.add("Authorization", "Bearer " + cookie.getValue());  // Add JWT to Authorization header
+                }
+            }
+        }
+    
+        // Create the HttpEntity with the request body and headers
+        HttpEntity<ResquestDTO> requestEntity = new HttpEntity<>(requestBody, headers);
+
+
+        if (selectedEvent.get_links() == null) {
+            url = "http://localhost:8080/api/evento";  
+
+            System.out.println("aquiiiiiiiiiiii");
+        
+            try {
+                // Make the POST request
+                ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+                System.out.println("Response: " + response.getStatusCode() + " - " + response.getBody());
+                // After POST, redirect to dashboard
+                FacesContext.getCurrentInstance().getExternalContext().redirect("dashboard.xhtml");
+            } catch (HttpClientErrorException e) {
+                System.out.println("Erro na requisição: " + e.getStatusCode() + " - " + e.getResponseBodyAsString());
+            } catch (Exception e) {
+                System.out.println("Erro inesperado: " + e.getMessage());
+            }
+    
+          
+            return;
+        }
+
+    
+        try {
+            // Make the PUT request
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, String.class);
+            System.out.println("Response: " + response.getStatusCode() + " - " + response.getBody());
+               // Redireciona para o dashboard
+               FacesContext.getCurrentInstance().getExternalContext().redirect("dashboard.xhtml");
+
+        } catch (HttpClientErrorException e) {
+            System.out.println("Erro na requisição: " + e.getStatusCode() + " - " + e.getResponseBodyAsString());
+        } catch (Exception e) {
+            System.out.println("Erro inesperado: " + e.getMessage());
+        }
+        System.out.println(url);
+        
+    }
 
 
 
     @PostConstruct
     public void init() {
+        selectedEvent = new EventDTO(); // Garante que não seja null
+       
+        
         eventModel = new LazyDataModel<EventDTO>() {
             @Override
             public List<EventDTO> load(int first, int pageSize, Map<String, SortMeta> sortBy, Map<String, FilterMeta> filterBy) {
@@ -81,6 +170,7 @@ public class EventoBean implements Serializable {
                         }
                     }
                 }
+               
 
 
                 HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
@@ -91,6 +181,7 @@ public class EventoBean implements Serializable {
                         ? response.getEmbedded().getEvento()
                         : Collections.emptyList();
 
+
                 // Apply the global filter manually
                 return applyGlobalFilter(events);
             }
@@ -98,10 +189,11 @@ public class EventoBean implements Serializable {
             @Override
             public int count(Map<String, FilterMeta> filterBy) {
                 if (getRowCount() > 0) {
+                    System.out.println("response");
                     return getRowCount();
                 }
 
-                String url = "http://localhost:8080/api/evento?page=0&size=1";
+                String url = "http://localhost:8080/api/evento?page=0&size=20";
 
                 RestTemplate restTemplate = new RestTemplate();
                 HttpHeaders headers = new HttpHeaders();
@@ -111,9 +203,10 @@ public class EventoBean implements Serializable {
                 HttpServletRequest req = (HttpServletRequest) fc.getExternalContext().getRequest();
                 if (req.getCookies() != null) {
                     for (Cookie cookie : req.getCookies()) {
-                        System.out.println("Cookie encontrado: " + cookie.getName() + " = " + cookie.getValue());
+                        
                         if ("jwt".equals(cookie.getName())) {
-                            headers.add("Cookie", "jwt=" + cookie.getValue());
+                            headers.add("Authorization", "Bearer " + cookie.getValue());
+
                         }
                     }
                 }
@@ -123,6 +216,7 @@ public class EventoBean implements Serializable {
                 ResponseEntity<EventoResponse> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, EventoResponse.class);
                 EventoResponse response = responseEntity.getBody();
 
+                System.out.println("respose" + response);
                 if (response != null && response.getPage() != null) {
                     setRowCount(response.getPage().getTotalElements());
                 }
---
+++ jsf-app/src/main/java/com/mobsolution/jsf_app/dto/ResquestDTO.java
@@ -0,0 +1,9 @@
+package com.mobsolution.jsf_app.dto;
+
+
+public record ResquestDTO(
+
+    String nome, 
+    String dataInicio,
+    String dataFim
+) {}
